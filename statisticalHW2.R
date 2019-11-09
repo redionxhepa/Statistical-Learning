fb0a58eddb2e2b0363810e8ef73777c6cd9ee15d
Redion Xhepa <33065889+redionxhepa@users.noreply.github.com>
2019-11-09 14:56:59 +0100

    Almost done just part 4 sth not clear but simple though

--- statisticalHW2.R
+++ statisticalHW2.R
@@ -2,10 +2,6 @@
 #Redion Xhepa and Anurag Das  #Matriculation Numbers
 #Elements of Statistrical Learning HW2
 #load the library
-
-#Redion Xhepa  ID:
-#Anurag Das   ID:
-
 library ( ISLR )
 #retrieve the dataset
 fix(Auto)
@@ -93,4 +89,51 @@ AutoWithoutName=Auto[,1:8] #discard the names column
 correlations=cor(AutoWithoutName,AutoWithoutName, use = "everything",
                  method = c("pearson"))
 print(correlations)
+#Question 4 part 3
+print("QUestion 4 part 3")  #One vs one all one vs three ?
+print("Linear Model with cylinders,horsepower,year")
+linearModel_cylinders=lm(mpg~cylinders+horsepower+year, data=AutoWithoutName)
+print(summary(linearModel_cylinders))
+
+print("Linear Model with cylinders")
+linearModel_cylinders=lm(mpg~cylinders, data=AutoWithoutName)
+print(summary(linearModel_cylinders))
+
+print("Linear Model with horsepower")
+linearModel_horsepower=lm(mpg~horsepower, data=AutoWithoutName)
+print(summary(linearModel_horsepower))
+
+print("Linear Model with year")
+linearModel_year=lm(mpg~year, data=AutoWithoutName)
+print(summary(linearModel_year))
+
+
+
+#Question 4 part 4
+print("QUestion 4 part 4")
+linearModel_multi=lm(mpg~., data=AutoWithoutName)
+print(summary(linearModel_multi))
+
+
+# #Question 4 part 5
+# print("QUestion 4 part 5")
+#  plot ( AutoWithoutName[] ,)
+#  abline (linearModel_multi)
+# 
+#  par(mfrow=c(2,2))
+#  plot( AutoWithoutName["mpg"], AutoWithoutName["mpg"] )
+#  abline(lm(wt ~ mpg, mtcars))
+#  plot(mtcars$disp,mtcars$wt)
+#  abline(lm(wt ~ disp, mtcars))
+
+
+#Question 4 part 5
+print("Question 4 part 5")
+model_cyl_weight_logX=lm(mpg~cylinders*weight+I(log(displacement)), data=AutoWithoutName)
+print("done A")
+model_cyl_year_squareRootX=lm(mpg~cylinders*year+I(sqrt(displacement)), data=AutoWithoutName)
+print("done B")
+model_weight_year_squareX=lm(mpg~weight*year+I(displacement^2), data=AutoWithoutName)
+print("done C")
+print(model_cyl_year_squareRootX)
 
